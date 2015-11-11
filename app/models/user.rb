c4b12f21342019821e821a7d24e73386ee189f03
Stefan Dorunga <stefandorunga@gmail.com>
2015-11-11 13:49:22 +0000

    Uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User
 
   has n, :peeps
 
+  validates_uniqueness_of :email
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
